61048d7350529ad9c5996fcf37945f684a3db680
ChristianEchivarre-UCM <christianechivarre27@gmail.com>
2025-09-03 08:51:15 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong{
+public static void main(String []args){
+System.out.println("Should i give up or should i just keep chasing pavements ");
+System.out.println("Even if it leads nowhere ");
+System.out.println("Or would it be a waste? ");
+System.out.println("Even if i know my place should i leave it there ");
+}
+}
