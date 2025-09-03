cb5e51af77415ecc9fa5e7c90b85b4d146ef07e4
ChristianEchivarre-UCM <christianechivarre27@gmail.com>
2025-09-03 06:18:26 -0400

    Create Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,7 @@
+public class Comments{
+public static void main(String[]args){
+System.out.println("Hello World "); // Single line comment
+System.out.println("Hello World "); /*Multi line comment */
+System.out.println("Hello World "); /**This is Document Comment*/
+}
+}
