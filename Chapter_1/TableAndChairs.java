128d9ae8432dda9a7802d418dcf75f68c463fd6b
ChristianEchivarre-UCM <christianechivarre27@gmail.com>
2025-09-03 08:47:41 +0800

    Create TableAndChairs.java

---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,12 @@
+public class TableAndChairs{
+public static void main(String []args ){
+for(int i = 0; i <= 7; i++){
+System.out.println("x                                        x");
+}
+System.out.println("__________   ______________    ___________");
+
+for(int i = 0; i <= 7; i++){
+System.out.println("x        x   x            x    x         x");
+}
+}
+}
