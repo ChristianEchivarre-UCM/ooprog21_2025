dcf3dcbb5082af387636f1cc31203ed41e71f810
ChristianEchivarre-UCM <christianechivarre27@gmail.com>
2025-09-07 22:11:34 -0400

    Create FixDebug.java

---
+++ Chapter_1/FixDebug.java
@@ -0,0 +1,25 @@
+public class FixDebug One{
+public static void Main(String []args{
+* This program displays a greeting */
+Systm.out.println("Hello World!")
+/* Heres the fix code of the given example above
+public class FixDebugOne{
+public static void main(String[]args){
+System.out.println("Hello World!"); //This program display the greetings
+}
+}
+*/
+
+/*
+Bugs:
+1. There's a space in naming the class instead of FixDebugOne
+2. The single line comment is misplaced and wrong syntax
+3. Missing semi colon after Hello World
+4.Typo in System insted of System
+5. the Main should be lower case and missing bracket [] after String
+6.
+*/
+
+}
+}
+}
