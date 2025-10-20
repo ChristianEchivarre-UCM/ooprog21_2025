987f753b09dc806f58c711d43bbbfeeec32fa5f9
ChristianEchivarre-UCM <christianechivarre27@gmail.com>
2025-10-19 21:02:53 -0400

    Create Loops.java

---
+++ chapter_6/Loops.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Loops {
+public static void main(String[] args) {
+final double FIXED_INTEREST = 0.03;
+Scanner s = new Scanner(System.in);
+
+System.out.print("Enter initial bank balance: ");
+double balance = s.nextDouble();
+
+int year = 1;
+int choice = 1;
+while (choice == 1){
+System.out.println("Do you want to see next year's balance?");
+System.out.print("Enter 1 for yes or any other number for no >> ");
+choice = s.nextInt();
+if(choice == 1){
+balance = balance + (balance * FIXED_INTEREST);
+System.out.printf("After year %d at 3%% interest, balance is $%.2f%n", year, balance);
+year++; 
+}
+else{
+s.close();
+}      
+}
+}
+}
